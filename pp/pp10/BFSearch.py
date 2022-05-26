76583122214edff92f65955d5bb140655ed05813
June <khsj15@khu.ac.kr>
2022-05-26 22:56:43 +0900

    push

--- pp/pp10/BFSearch.py
+++ pp/pp10/BFSearch.py
@@ -7,3 +7,20 @@ def breadth_first_search(graph, startVertex, endVertex):
 
     
     '''[10]'''
+    vertex = startVertex
+    graph.clear_marks()
+    while not found :
+        print(vertex)
+        found = vertex is endVertex
+        if (found) :
+            return
+        fIdx = graph.vertices.index(vertex)
+        tIdx = 0
+        graph.mark_vertex(vertex)
+        for i in range (0, graph.numVertices):
+            if graph.edges[fIdx][tIdx] is not 0 :
+                queue.enqueue(graph.vertices[tIdx])
+        if (queue.is_empty()):
+            return
+        vertex = queue.dequeue()
+        
--- pp/pp10/GraphType.py
+++ pp/pp10/GraphType.py
@@ -24,32 +24,63 @@ class GraphType:
 
     def add_vertex(self, vertex):
         '''[1]'''
+        if (self.maxVertices <= self.numVertices) :
+            return 
+        self.vertices[self.numVertices] = vertex
+        self.numVertices += 1
 
     def add_edge(self, fromVertex, toVertex, weight):
         '''[2]'''
-
+        if (fromVertex not in self.vertices or toVertex not in self.vertices) :
+            return
+        fIdx = self.vertices.index(fromVertex)
+        tIdx = self.vertices.index(toVertex)
+        self.edges[fIdx][tIdx] = weight
 
     def weight_is(self, fromVertex, toVertex):
         '''[3]'''
-
+        if (fromVertex not in self.vertices or toVertex not in self.vertices) :
+            return
+        fIdx = self.vertices.index(fromVertex)
+        tIdx = self.vertices.index(toVertex)
+        return self.edges[fIdx][tIdx]
 
     def get_to_vertices(self, vertex, adjVertices):
         '''[4]'''
-
-
+        if (vertex not in self.vertices) :
+            return
+        tIdx = self.vertices.index(vertex)
+        if (tIdx == -1) :
+            return
+        idx = 0 
+        while (idx < self.numVertices) :
+            if (self.edges[idx][tIdx] is not 0) :
+                adjVertices.append(self.vertices[idx])
+            idx += 1
 
     def clear_marks(self):
         '''[5]'''
-
+        for mark in self.marks :
+            mark = None
 
     def is_marked(self, vertex):
         '''[6]'''
-
+        if (vertex not in self.vertices) :
+            return
+        tIdx = self.vertices.index(vertex)
+        return (self.marks[tIdx] != None)
 
     def mark_vertex(self, vertex):
         '''[7]'''
-
+        if (vertex not in self.vertices) :
+            return
+        tIdx = self.vertices.index(vertex)
+        self.marks[tIdx] != True
 
     def delete_edge(self, fromVertex, toVertex):
         '''[8]'''
-
+        if (fromVertex not in self.vertices or toVertex not in self.vertices) :
+            return
+        fIdx = self.vertices.index(fromVertex)
+        tIdx = self.vertices.index(toVertex)
+        self.edges[fIdx][tIdx] = None
